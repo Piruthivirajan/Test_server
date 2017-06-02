2a6cadb44308d41be14e52d5ee8b62171389da9f
Piruthivirajan <piruthivi32@gmail.com>
2017-06-02 17:53:57 +0530

    Update UserRepository.java

--- src/main/java/com/example/Server_side/UserRepository.java
+++ src/main/java/com/example/Server_side/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.Server_side;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.model.User;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
 
