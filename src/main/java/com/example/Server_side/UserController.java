245aed370b5d359fcd2fed0576ae31af63f99efd
Piruthivirajan <piruthivi32@gmail.com>
2017-06-02 17:47:14 +0530

    Update UserController.java

--- src/main/java/com/example/Server_side/UserController.java
+++ src/main/java/com/example/Server_side/UserController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+	@Autowired
 	private UserRepository userresository;
 	@RequestMapping(method = RequestMethod.GET)
 	public List<User> findall() {
